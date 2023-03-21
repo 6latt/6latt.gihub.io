b01710e7071fc8f1355fc9c3e4ecbb14ab41f128
6latt <103127735+6latt@users.noreply.github.com>
2023-03-21 15:53:11 -0400

    potential css and scripts from aino i might use

---
+++ _next/static/chunks/pages/careers-cb7e14e9ddbb8979.js
@@ -0,0 +1,88 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [698],
+  {
+    4931: function (e, n, s) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        "/careers",
+        function () {
+          return s(2305);
+        },
+      ]);
+    },
+    2305: function (e, n, s) {
+      "use strict";
+      s.r(n);
+      var o = s(5893),
+        i = s(4808),
+        a = s.n(i),
+        r = s(830),
+        t = s.n(r),
+        l = s(4019),
+        d = s(7873),
+        c = s(3848);
+      let u = () =>
+        (0, o.jsxs)("div", {
+          className: a().container,
+          children: [
+            (0, o.jsx)(l.Z, { title: "Careers", slug: "careers" }),
+            (0, o.jsxs)("div", {
+              className: t().row26,
+              children: [
+                (0, o.jsx)("div", {}),
+                (0, o.jsx)("div", {
+                  className: "preamble",
+                  children: (0, o.jsx)("p", {
+                    children:
+                      "We are always looking for talent. If you are passionate about something, like code, art or design, or simply got fed up working for toxic employers (and you should) — talk to us.",
+                  }),
+                }),
+                (0, o.jsx)("div", {}),
+              ],
+            }),
+            (0, o.jsx)("div", { className: t().space1 }),
+            (0, o.jsx)("div", {
+              className: t().row44,
+              children: (0, o.jsxs)("div", {
+                className: ["text", t().col2].join(" "),
+                children: [
+                  (0, o.jsx)("div", {
+                    children: (0, o.jsx)("p", {
+                      children:
+                        "Aino is a new-wave design and technology agency, comprised of colleagues turned friends that genuinely enjoy working together. We are on a mission to explore the creative playground of code, design, and branding for ambitious clients in Scandinavia and the World.",
+                    }),
+                  }),
+                  (0, o.jsx)("p", {
+                    children:
+                      "We welcome both digital designers, motion designers, UX designers, developers and agency managers to connect with us for employment or collaborations. It doesn’t really matter if you are a junior, senior, or part-time student — as long as you are ambitious, passionate, and share our values and pursuit for quality.",
+                  }),
+                  (0, o.jsxs)("p", {
+                    children: [
+                      "Send your portfolio and CV to",
+                      " ",
+                      (0, o.jsx)("strong", {
+                        children: (0, o.jsx)("a", {
+                          href: "mailto:linnea@aino.agency",
+                          children: "linnea@aino.agency",
+                        }),
+                      }),
+                      " ",
+                      "with some words about why you want to work with us. Even if the position isn’t listed below, we accept open applications at any time.",
+                    ],
+                  }),
+                ],
+              }),
+            }),
+            (0, o.jsx)(c.Z, {}),
+            (0, o.jsx)(d.Z, {}),
+          ],
+        });
+      n.default = u;
+    },
+  },
+  function (e) {
+    e.O(0, [675, 515, 534, 774, 888, 179], function () {
+      return e((e.s = 4931));
+    }),
+      (_N_E = e.O());
+  },
+]);
